39fb3c7e20217050f5279587eed7fc45a12840cc
Paulo Grácio <paulo.gracio@klarna.com>
2016-06-24 00:46:44 +0200

    #9 Option Set tests - Modify an option set type.

--- modules/data-admin/optionSet.js
+++ modules/data-admin/optionSet.js
@@ -78,6 +78,24 @@ describe("DHIS2 API - Data Administration - Option Set", function () {
             return chakram.wait();
         });
 
+        it("shouldn't be able to update existent Option Set type from 'INTEGER' to 'TEXT'", function () {
+            this.timeout(10000);
+            var optionSetResponse = chakram.get(env.url + "/api/optionSets?filter=code:eq:TEST_OPTION_SET_NEW", env.auth);
+
+            expect(optionSetResponse).to.have.json(function (json) {
+                var optionSetId = json.optionSets[0].id;
+                var payload = {name: 'Test Option Set New', valueType: 'TEXT'};
+                var response = chakram.put(env.url + env.api + "optionSets/" + optionSetId, payload, env.auth);
+
+                // TODO: currently is possible to change types...
+                expect(response).to.have.status(400);
+                //expect(response).to.have.json('response.importCount.updated', 1);
+
+                return chakram.wait();
+            });
+            return chakram.wait();
+        });
+
         it("should be able to create a new Option Set named 'Test Option Set02' with Option", function () {
             this.timeout(10000);
             var payload = {
